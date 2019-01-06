3a7174f1bcb92fca1c315209f38eeeb1fd9f8223
Nelson (Nelx) Secco Filho <nelsonseccofilho@gmail.com>
2019-01-06 14:34:13 -0200

    Seção 01 - aula 07

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # introducao-a-linguagem-javascript
 Curso para aprender programação front-end usando a linguagem JavaScript
+
+##O que você aprenderá
+- Criar os primeiros scripts com JavaScript
+- Compreender o DOM
+- Utilizar eventos
+- Criar funções
+
+link do curso: [https://www.udemy.com/introducao-a-linguagem-javascript/]: https://www.udemy.com/introducao-a-linguagem-javascript/
+
+ 
---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var a = 2;
+var b = 3;
+
+var soma = a + b;
+var sub = a - b;  
+var mult = a * b;
+var div = a / b;
+
+// alert(soma);
+// alert(sub);
+// alert(mult);
+// alert(div);
+
+// mótulo, apresenta o valor do resto
+// alert(8%2);
+
+// operador de atribuição
+var a = x
+// operador de comparação
+var a == x
+
+// operador de comparação de valor e tipo
+var a === x
+
+//estruturas condicionais
+if (b % 2 == 1) {
+  alert('número ímpar')
+}
